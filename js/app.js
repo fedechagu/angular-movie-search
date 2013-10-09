0981b8e40db760c94bff8de640de4a4d7d0d72da
Federico Chapa <federicochapa@provenmethod.com>
2013-10-08 22:52:02 -0400

    initial setup

---
+++ js/app.js
@@ -0,0 +1,30 @@
+﻿
+var AngularJsDemo = angular.module('AngularJsDemo', ['ngResource']);
+
+AngularJsDemo.factory('Movies', function ($http) {
+    var imdbapi;
+    this.baseUrl = 'http://mymovieapi.com/';
+  
+    var url = this.baseUrl + '?title=batman&type=jsonp&plot=full&episode=1&limit=10&yg=0&mt=none&lang=en-US&offset=&aka=simple&release=simple&business=0&tech=0';
+    this.imdbapi = function () { };
+    this.getMoviesAsync = function (callback, imbdapi) {
+        $http.jsonp(url).done(function (callback) {
+            debugger;
+        });
+    }
+    return this
+});
+
+
+function SearchController($scope, $resource) {
+
+    var MovieList = $resource('http://mymovieapi.com/', {
+        'title': 'batman',
+        'type': 'jsonp',
+        'limit': '10'
+    });
+
+    $scope.data.movies = MovieList.get();
+};
+
+
